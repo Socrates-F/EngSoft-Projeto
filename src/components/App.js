7d957b017fc9fd76190ff84c4d79823bedb40ccd
Socrates-F <socrates.cratos09@gmail.com>
2023-12-19 05:11:38 -0300

    Modificação Interface

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Logo no canto superior esquerdo */}
-        <img src="caminho/do/seu/logo.png" alt="Logo" className="logo" />
-
-        {/* Botões interativos */}
-        <div className="botoes-container">
-          <button className="botao">Botão 1</button>
-          <button className="botao">Botão 2</button>
-          <button className="botao">Botão 3</button>
+        <img src="/logo.png" className="App-logo" alt="Logo" />
+        <div className="Button-container">
+          <button className="App-button">Botão 1</button>
+          <button className="App-button">Botão 2</button>
+          <button className="App-button">Botão 3</button>
         </div>
       </header>
     </div>
